9dde8201f50db119bbe83a55672a5d5d7b27772b
hoanghieu2385 <hoangminhhieu230805@gmail.com>
2024-11-05 22:08:40 +0700

    xoá phương thức paypal

--- BestStoreMVC/BestStoreMVC/Controllers/CartController.cs
+++ BestStoreMVC/BestStoreMVC/Controllers/CartController.cs
@@ -64,12 +64,6 @@ namespace BestStoreMVC.Controllers
             TempData["DeliveryAddress"] = model.DeliveryAddress;
             TempData["PaymentMethod"] = model.PaymentMethod;
 
-
-            if (model.PaymentMethod == "paypal" || model.PaymentMethod == "credit_card")
-            {
-                return RedirectToAction("Index", "Checkout");
-            }
-
             return RedirectToAction("Confirm");
         }
 
